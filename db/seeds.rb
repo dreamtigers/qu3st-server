a3216a63552fab91a3ba116ed570d0d962eb7ca8
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2018-11-26 16:56:24 -0400

    Add seed for base team

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p 'Creating team MAC'
+Team.create(username: 'MAC', password: 'dale con el bate', password_confirmation: 'dale con el bate')
