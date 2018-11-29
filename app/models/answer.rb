3242f9df192fa36b85e2144b49d8d851f78f17f1
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2018-11-29 00:47:45 -0400

    Add Team-question unique validation for Answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,7 @@
 class Answer < ApplicationRecord
+  validates_uniqueness_of :question_id, scope: :team_id, message: "There's an
+  existing answer for this question, cannot create a new one."
+
   belongs_to :team
   belongs_to :question
 end
