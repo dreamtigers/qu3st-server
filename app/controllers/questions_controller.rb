6f95c70e6280e192f67f5c619e413dc73d46b91e
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2018-11-29 03:14:07 -0400

    Add questions controller
    It has two routes: index, which will show all the questions, and show,
    which will show only one question with its details.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,25 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+  # GET /questions
+  # GET /questions.json
+  def index
+    @questions = Question.all
+  end
+
+  # GET /questions/1
+  # GET /questions/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def question_params
+      params.require(:question).permit(:description, :key)
+    end
+end
