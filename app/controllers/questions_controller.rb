8d19393d251982c86aa196344fabf59627a60841
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2018-12-04 13:12:12 -0400

    Update Question MVC and make it manage Answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_team!
-  before_action :set_question, only: [:show]
+  before_action :set_question, only: [:show, :update]
 
   # GET /questions
   # GET /questions.json
@@ -12,8 +12,27 @@ class QuestionsController < ApplicationController
   def show
   end
 
+  # PATCH/PUT /questions/1
+  # PATCH/PUT /questions/1.json
+  def update
+    respond_to do |format|
+      if @question.check_key(question_params, current_team)
+        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @question }
+      else
+        format.html { render :show }
+        # format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def set_question
       @question = Question.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def question_params
+      params.require(:question).permit(:key)
     end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,17 @@
 class Question < ApplicationRecord
+  has_many :answers
+  has_many :teams, through: :answers
+
+  def check_key params, team
+    if params[:key] == self.key
+      complete = true
+    elsif params[:key] == 'run'
+      complete = false
+    else
+      return false
+    end
+    # answer = Answer.find_or_initialize_by(team_id: team_id, question_id: self.id)
+    # answer.update(complete: complete)
+    team.answers.find_or_create_by(question_id: self.id).update(complete: complete)
+  end
 end
