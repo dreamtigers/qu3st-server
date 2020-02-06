49fb53fdc708c2f12a6ce59415e4ddf72a5697e3
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-02-06 00:46:51 -0400

    Fix bug that didn't load the next-question view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_team!
   before_action :set_question, only: [:show, :update]
-  before_action :set_next_question, only: [:show, :update]
 
   # GET /questions
   # GET /questions.json
@@ -11,8 +10,8 @@ class QuestionsController < ApplicationController
   # GET /questions/1
   # GET /questions/1.json
   def show
-    if params[:id].to_i > @next_question.id
-      redirect_to @next_question, notice: ("You can't go to question %02d yet." % params[:id])
+    if params[:id].to_i > current_team.next_question.id
+      redirect_to current_team.next_question, notice: ("You can't go to question %02d yet." % params[:id])
     end
   end
 
@@ -21,7 +20,7 @@ class QuestionsController < ApplicationController
   def update
     respond_to do |format|
       if @question.check_key(question_params[:key], current_team)
-        format.html { redirect_to @next_question, notice: 'Question was successfully updated.' }
+        format.html { redirect_to current_team.next_question, notice: 'Question was successfully updated.' }
         # format.json { render :show, status: :ok, location: @question }
       else
         format.html { render :show }
@@ -33,10 +32,6 @@ class QuestionsController < ApplicationController
   private
     def set_question
       @question = Question.find(params[:id])
-    end
-
-    def set_next_question
-      @next_question = current_team.next_question
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
